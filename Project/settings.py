a7cba5eb34d2a753f794ff26f3e5de3a11782f91
Mgrestoo <abiudmagora@gmail.com>
2026-04-25 03:25:25 -0700

    Added a .env file to hide secret key

--- Project/settings.py
+++ Project/settings.py
@@ -10,23 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sg#2&cna=uh=8bfoijt@99kfm!cboashd#oyawb=+6-++uvg3c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','FALSE')== True
 
 ALLOWED_HOSTS = ['mgrestoo.pythonanywhere.com','localhost','127.0.0.1']
-# ALLOWED_HOSTS = ['']
 
 
 
